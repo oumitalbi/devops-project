f1b3a80276505041996b3688376230a363217f08
Oumaima Talbi <oumatalbi352@gmail.com>
2026-04-02 13:38:53 +0200

    test

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+# tests/test_app.py
+
+def test_always_passes():
+    """Un test simple pour vérifier que le pipeline fonctionne"""
+    assert True
+
+def test_logic():
+    """Un exemple de test logique"""
+    valeur = 10
+    assert valeur + 5 == 15
